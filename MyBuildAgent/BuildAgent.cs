adc7a0fdad1adda76933ee3f9e3c7621c6962198
bhp <bhp@easv.dk>
2019-01-29 12:14:50 +0100

    Add project files.

---
+++ MyBuildAgent/BuildAgent.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace MyBuildAgent
+{
+    class BuildAgent
+    {
+        public string Id { get; private set; }
+        public string BuildPath { get; private set; }
+        public int Time { get; private set; }
+
+        private bool done;
+        private Thread t = null;
+
+        public BuildAgent(string id, string buildPath, int timeInSeconds)
+        {
+            Id = id;
+            BuildPath = buildPath;
+            Time = timeInSeconds;
+            t = new Thread(() => Run());
+            t.Start();
+        }
+
+        public void Cancel()
+        {
+            t.Interrupt();
+            done = true;
+        }
+
+        private void Run()
+        {
+            done = false;
+            while (!done)
+            {
+
+                Console.WriteLine($"{Id} building...");
+
+                Process p = new Process
+                {
+                    StartInfo =
+                    {
+                        UseShellExecute = false,
+                        RedirectStandardOutput = false,
+                        FileName = "dotnet",
+                        Arguments = $" build {BuildPath}"
+                    }
+                };
+                p.Start();
+                p.WaitForExit();
+                try
+                {
+                    Thread.Sleep(Time * 1000);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    // Do nothing special - just wake up!
+                }
+            }
+
+        }
+    }
+}
---
+++ MyBuildAgent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace MyBuildAgent
+{
+    class Program
+    {
+        const string BuildPath = @"C:\Users\bhp\source\repos\AA\Week5\MyBuildAgent";
+
+        static void Main(string[] args)
+        {
+            BuildAgent agent1 = new BuildAgent("Agent1", BuildPath, 5);
+            BuildAgent agent2 = new BuildAgent("Agent2", BuildPath, 8);
+
+            Console.WriteLine("Press any key to stop Agent1");
+            Console.ReadKey();
+
+            agent1.Cancel();
+
+            Console.WriteLine("Press any key to stop Agent2");
+            Console.ReadKey();
+        
+            agent2.Cancel();
+        }
+    }
+}
