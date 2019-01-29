e4f8644587271fcc47599b61733a7fcc60408b15
bhp <bhp@easv.dk>
2019-01-29 16:22:31 +0100

    Refactored the code

--- MyBuildAgent/BuildAgent.cs
+++ MyBuildAgent/BuildAgent.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Threading;
+using ThreadState = System.Threading.ThreadState;
 
 namespace MyBuildAgent
 {
     class BuildAgent
     {
+        private static readonly object buildLock = new object();
+
         public string Id { get; private set; }
         public string BuildPath { get; private set; }
         public int Time { get; private set; }
+        public bool IsCanceled { get; private set; }
 
-        private bool done;
+        private Process buildProcess;
         private Thread t = null;
 
         public BuildAgent(string id, string buildPath, int timeInSeconds)
@@ -18,46 +22,69 @@ namespace MyBuildAgent
             Id = id;
             BuildPath = buildPath;
             Time = timeInSeconds;
+            buildProcess = ConfigureBuildProcess();
+
             t = new Thread(() => Run());
             t.Start();
         }
 
         public void Cancel()
         {
-            t.Interrupt();
-            done = true;
+            IsCanceled = true;
+            if (t.ThreadState == ThreadState.WaitSleepJoin)
+                t.Interrupt();
         }
 
         private void Run()
         {
-            done = false;
-            while (!done)
+            IsCanceled = false;
+            while (!IsCanceled)
             {
+                int exitCode = DoBuild();
 
-                Console.WriteLine($"{Id} building...");
-
-                Process p = new Process
+                if (!IsCanceled)
                 {
-                    StartInfo =
-                    {
-                        UseShellExecute = false,
-                        RedirectStandardOutput = false,
-                        FileName = "dotnet",
-                        Arguments = $" build {BuildPath}"
-                    }
-                };
-                p.Start();
-                p.WaitForExit();
-                try
-                {
-                    Thread.Sleep(Time * 1000);
-                }
-                catch (ThreadInterruptedException)
-                {
-                    // Do nothing special - just wake up!
+                    TimedDelay(Time);
                 }
             }
+        }
 
+        private Process ConfigureBuildProcess()
+        {
+            return new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = false,
+                    FileName = "dotnet",
+                    Arguments = $" build {BuildPath}"
+                }
+            };
+        }
+
+        private int DoBuild()
+        {
+            lock (buildLock)
+            {
+                Console.WriteLine($"{Id} building...");
+                buildProcess.Start();
+                buildProcess.WaitForExit();
+                return buildProcess.ExitCode;
+            }
+        }
+
+        private void TimedDelay(int time)
+        {
+            try
+            {
+                Thread.Sleep(Time * 1000);
+            }
+            catch (ThreadInterruptedException)
+            {
+                Console.WriteLine($"{Id} Got Interrupted!");
+                // Do nothing special - just wake up!
+            }
         }
     }
 }
